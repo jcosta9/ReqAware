db49a9ec8fc7c2452ba908cf2ac32bf035f1fe14
Joao Paulo Costa de Araujo <jp.araujo@hu-berlin.de>
2025-08-20 18:04:03 +0200

    Script for training a cbm

---
+++ src/train_cbm.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from omegaconf import OmegaConf
+
+from models.architectures import CBMSequentialEfficientNetFCN
+from config import CBMTrainerConfig
+from models.trainer.cbm_trainer import CBMTrainer
+
+
+def load_config(config_path):
+    """
+    Load the configuration from a YAML file.
+    """
+    # Load YAML
+    cfg_yaml = OmegaConf.load(config_path)
+    cfg_structured = OmegaConf.structured(CBMTrainerConfig)
+    cfg = OmegaConf.merge(cfg_structured, cfg_yaml)
+    cfg = OmegaConf.to_object(cfg)
+    cfg.resolve()
+    return cfg
+
+
+def main():
+    config = load_config(Path("../files/configs/GTSRB_CBM_config.yaml"))
+
+    # Dataset
+    dataset_factory = config.dataset.factory(config).set_dataloaders()
+
+    if dataset_factory is None:
+        return
+
+    train_loader = dataset_factory.train_dataloader
+    val_loader = dataset_factory.val_dataloader
+    test_loader = dataset_factory.test_dataloader
+
+    # # Model
+    model = CBMSequentialEfficientNetFCN(config)
+
+    # # Train
+    trainer = CBMTrainer(
+                config=config,
+                model=model,
+                train_loader=train_loader, 
+                val_loader=val_loader, 
+                test_loader=test_loader
+            )
+    trainer.train()
+
+    # return dataset_factory, model, trainer
+
+
+if __name__ == "__main__":
+    main()
