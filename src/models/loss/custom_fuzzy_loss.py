c71a4a4711010a094a0f4c30261fe09904aa4c83
Eik Reichmann <eik.reichmann@gmail.com>
2025-08-28 13:33:12 +0200

    [Model] Implement ExactlyOneShapeGodel loss function using Godel t-norm transformations

--- src/models/loss/custom_fuzzy_loss.py
+++ src/models/loss/custom_fuzzy_loss.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from fuzzy_transformations import GodelTNorm, GodelAAggreation, GodelEAggregation
 
 
 class CustomFuzzyLoss(nn.Module):
@@ -33,3 +34,34 @@ class CustomFuzzyLoss(nn.Module):
         # Combine the losses
         # total_loss = standard_loss + self.fuzzy_lambda * fuzzy_loss
         return total_loss
+
+class ExactlyOneShapeGodel(nn.Module):
+    """A loss function resembling that a traffic sign has exactly one shape in the GTSRB using the Godel t-norm transformation."""
+    def __init__(self, config, current_loss_fn, shape_indices):
+        super().__init__()
+        self.config = config
+        self.current_loss_fn = current_loss_fn
+        self.fuzzy_lambda = config.fuzzy_lambda
+        self.shape_concept_indices = shape_indices
+
+        for rule_fn in self.config.custom_rules:
+            print(f"Using custom fuzzy rule: {rule_fn.__name__}")
+    
+
+    def forward(self, y_pred: torch.tensor, y_true: torch.tensor) -> torch.tensor:
+        """Implementation of the exactly one shape loss using the Godel t-norm"""
+        standard_loss = self.current_loss_fn(y_pred, y_true)
+        exist_aggregation = torch.tensor(device=self.config.device + ":" + self.config.device_no)
+        all_tnorm_values = []
+        for i in range(len(self.shape_concept_indices)):
+            # calculating the inner most t-norm aggreation where the shapes are unequal: (∀c): min_{j!=i}(1 - prob_j(c))
+            inner_most_probs = torch.cat([y_pred[:, :i], y_pred[:, i+1:]], dim=1)
+            inner_most_values = 1 - inner_most_probs
+            inner_most_tnorm = GodelAAggreation(inner_most_values)
+            # calculating the t-norm between the shape i and the innermost aggregation
+            tnorm_value = GodelTNorm(y_pred[i] , inner_most_tnorm)
+            all_tnorm_values.append(tnorm_value)
+        
+        # final exist aggregation step
+        fuzzy_loss = 1 - GodelEAggregation(torch.tensor(all_tnorm_values))
+        return standard_loss + self.fuzzy_lambda * fuzzy_loss
