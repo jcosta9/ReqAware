57360c179d1fd9d66de4f902e63b335acc27dc6f
Eik Reichmann <eik.reichmann@gmail.com>
2025-08-28 10:21:10 +0200

    [Model] Add Godel fuzzy transformations for T-norm, T-conorm, and aggregation

---
+++ src/models/loss/fuzzy_transformations.py
@@ -0,0 +1,18 @@
+from abstract.fuzzy_transformation import Tnorm, Tconorm, Implication, Aggregation
+import torch
+
+class GodelTNorm(Tnorm):
+    def forward(self, a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+        return torch.min(a, b)
+
+class GodelTConorm(Tconorm):
+    def forward(self, a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+        return torch.max(a, b)
+    
+class GodelEAggregation(Aggregation):
+    def forward(self, *inputs) -> torch.tensor:
+        torch.max(inputs)
+
+class GodelAAggreation(Aggregation):
+    def forward(self, *inputs) -> torch.tensor:
+        return torch.min(inputs)
