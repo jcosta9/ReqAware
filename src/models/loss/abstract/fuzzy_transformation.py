5bd126bf8b0e87a5fe60a2b1b1a11e72ccce3051
Eik Reichmann <eik.reichmann@gmail.com>
2025-08-28 10:20:56 +0200

    [Model] Implement abstract base classes for fuzzy transformations

---
+++ src/models/loss/abstract/fuzzy_transformation.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+from abc import ABC, abstractmethod
+
+class FuzzyTransformation(nn.Module, ABC):
+    """Abstract base class for Fuzzy transformation. Intented to implement the open close principals."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @abstractmethod
+    def forward(self, *inputs: torch.tensor) -> torch.tensor:
+        pass
+
+class Tnorm(FuzzyTransformation):
+    """Abstract class for all conjunctive 'and' fuzzy logic transformations, called t-norm"""
+    pass
+
+class Tconorm(FuzzyTransformation):
+    """Abstract class for all conjunctive 'or' fuzzy logic transformations, called t-conorm"""
+    pass
+
+class Implication(FuzzyTransformation):
+    """Abstract class for all implication fuzzy logic transformations. Either derived from t-norm or t-conorm"""
+    pass
+
+class Aggregation(FuzzyTransformation):
+    """Abstract class for all aggregation opperations in fuzzy logic"""
+    pass
+
