57bef533f7203bcb50d761c60bbd9e2ed0f3768b
Bruno Vincent Hemoura <hemourab@gruenau7.informatik.hu-berlin.de>
2026-08-04 10:56:47 +0200

    good working version for testing rho and lr in lagrangian_optimization

--- src/lagrangian_comparison.py
+++ src/lagrangian_comparison.py
@@ -1,11 +1,10 @@
 import os
+import time
 from pathlib import Path
-from datetime import datetime
 import yaml
 import subprocess
+import multiprocessing as mp
 from multiprocessing import Process
-
-running = {}
 
 import torch
 
@@ -17,17 +16,18 @@ from models.trainer.cbm_trainer import CBMTrainer
 rho_list = [0.001, 0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25]
 lr_list = [0.001, 0.0025, 0.005, 0.0075, 0.01]
 
-gpu_in_use = []
+gpu_in_use = {}
 
 def try_gpus():
     pass
 
+
+# RACE CONDITION!!!!
 def get_available_gpus():
-    max_memory_mb = 1000
     result = subprocess.check_output(
         [
             "nvidia-smi",
-            "--query-gpu=index,memory.used",
+            "--query-gpu=index,memory.free",
             "--format=csv,noheader,nounits",
         ]
     ).decode()
@@ -36,22 +36,35 @@ def get_available_gpus():
 
     for line in result.strip().split("\n"):
         idx, mem = line.split(",")
-        if int(mem) < max_memory_mb:
+        if int(mem) > 10000:
             free.append(int(idx))
 
     return free
 
+
 def assign_gpu():
-    pass
+    for gpu in get_available_gpus():
+#
+ #       if gpu not in gpu_in_use:
+  #          return gpu
+#
+ #       if not gpu_in_use[gpu].is_alive():
+  #          gpu_in_use[gpu].join()
+   #         del gpu_in_use[gpu]
+            return gpu
+
+    return None
 
 
 
-def execute_experiment(dataset_factory, experiment_id: int, rho: float, lr:float):
+def execute_experiment(experiment_id: int, rho: float, lr: float, gpu: int):
 
+    # RACE CONDITION!!!!
     with open("files/configs/GTSRB_CBM_config_best_trial_loading.yaml", "r") as f:
         config = yaml.safe_load(f)
 
     config["experiment_id"] = f"id: {experiment_id}, rho: {rho}, lr: {lr}"
+    config["device_no"] = gpu
 
     with open("files/configs/GTSRB_CBM_config_best_trial_loading.yaml", "w") as f:
         yaml.safe_dump(config, f, sort_keys=False)
@@ -63,6 +76,11 @@ def execute_experiment(dataset_factory, experiment_id: int, rho: float, lr:float
     config_cbm.concept_predictor.fuzzy_loss.use_lagrangian_optimization = True
     config_cbm.concept_predictor.fuzzy_loss.rho = rho
     config_cbm.concept_predictor.lr = lr
+    config_cbm.concept_predictor.gpu = gpu
+
+    dataset_factory = config_cbm.dataset.factory(
+        seed=config_cbm.seed, config=config_cbm.dataset
+    ).set_dataloaders()
 
     train_loader = dataset_factory.train_dataloader
     val_loader = dataset_factory.val_dataloader
@@ -85,22 +103,45 @@ def execute_experiment(dataset_factory, experiment_id: int, rho: float, lr:float
 
 
 def main():
-    config_cbm = load_config(Path("files/configs/GTSRB_CBM_config_best_trial_loading.yaml"))
-
-    dataset_factory = config_cbm.dataset.factory(
-        seed=config_cbm.seed, config=config_cbm.dataset
-    ).set_dataloaders()
-
-    if dataset_factory is None:
-        return
-
+    jobs = []
 
     experiment_id = 0
     for rho in rho_list:
         for lr in lr_list:
-            execute_experiment(dataset_factory, experiment_id, rho, lr)
+            jobs.append((experiment_id, rho, lr))
+            # execute_experiment(dataset_factory, experiment_id, rho, lr)
+            experiment_id += 1
+    
+    while jobs or gpu_in_use:
+
+        gpu = assign_gpu()
+
+        # if there is a gpu free and there are still jobs
+        if gpu is not None and jobs:
+
+            exp_id, rho, lr = jobs.pop(0)
+
+            p = Process(
+                target=execute_experiment,
+                args=(exp_id, rho, lr, gpu),
+            )
+
+            # mark gpu as in_use
+            #gpu_in_use[gpu] = p
+
+            p.start()
+
+
+            print(f"Started experiment {exp_id} on GPU {gpu}")
+
+            # lower risks of race condtions by using sleep()
+            time.sleep(50)
+
 
 
 if __name__ == "__main__":
-    # main()
+    print("starting main")
     print(get_available_gpus())
+
+    mp.set_start_method("spawn", force=True)
+    main()
